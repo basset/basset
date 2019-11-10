4b7f59f42ebb25e912c36559228140888f0da170
jeremy <jeremygonzalez@outlook.com>
2019-11-09 17:09:21 -0800

    remove leading slash from key

--- express/app/routes/snapshots.js
+++ express/app/routes/snapshots.js
@@ -10,7 +10,7 @@ router.get('*', async (req, res) => {
   if (token !== settings.token) {
     return res.send(404);
   }
-  const key = req.params[0];
+  const key = req.params[0].slice(1);
   const stream = s3
     .getObject({
       Bucket: settings.s3.assetsBucket,
@@ -18,7 +18,10 @@ router.get('*', async (req, res) => {
     })
     .createReadStream();
 
-  stream.on('error', err => res.status(400).json({ error: err.message }));
+  stream.on('error', err => {
+    console.error(err);
+    res.status(400).json({error: err.message});
+  });
 
   return stream.pipe(res);
 });
