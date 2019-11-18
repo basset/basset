0a7923da3741278a4ed4b5b5aacd5b9253aa268c
jeremy <jeremygonzalez@outlook.com>
2019-11-17 21:20:53 -0800

    update tests

--- express/tests/unit/tasks/queueCompareSnapshots.test.js
+++ express/tests/unit/tasks/queueCompareSnapshots.test.js
@@ -1,16 +1,17 @@
 let mockSendToQueue = jest.fn(async () => {});
-jest.mock('amqp-connection-manager', () => ({
-  connect: jest.fn(() => ({
+
+jest.mock('../../../app/utils/amqpConnection', () => ({
+  connection: {
     createChannel: jest.fn(() => ({
       sendToQueue: mockSendToQueue,
     })),
-  })),
-}));
-
+  }
+}))
 const tasks = require('../../../app/tasks/queueCompareSnapshots');
 
 describe('queueCompareSnapshots', () => {
   test('it sends messages to amqplib', async () => {
+    tasks.configureQueue();
     const messages = [{ test: 1 }, { test: 2 }, { test: 3 }];
     await tasks.queueCompareSnapshots(messages);
     expect(mockSendToQueue).toHaveBeenCalledTimes(3);
