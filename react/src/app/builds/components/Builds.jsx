1cf59530e134b9dd18f84e0c362c6fb3d8629cbb
jeremy <jeremygonzalez@outlook.com>
2019-11-30 19:26:14 -0800

    display alert when snapshots exceeded

--- react/src/app/builds/components/Builds.jsx
+++ react/src/app/builds/components/Builds.jsx
@@ -1,33 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Box, Text, Button } from 'grommet';
-import { Update } from 'grommet-icons';
+import { Box, Button, Text } from 'grommet';
+import { StatusWarning, Update } from 'grommet-icons';
 
 import {
   getBuilds,
+  getCurrentBuild,
+  getHasNextPage,
   getIsLoading,
   getIsLoadingMore,
-  getHasNextPage,
-  getCurrentBuild,
 } from '../../../redux/builds/selectors.js';
-import {
-  changeBuild,
-  loadMore,
-  setCurrentBuild,
-  getBuilds as reloadBuilds,
-} from '../../../redux/builds/actions.js';
+import { getCurrentOrganization } from '../../../redux/organizations/selectors.js';
 
+import { changeBuild, getBuilds as reloadBuilds, loadMore, setCurrentBuild } from '../../../redux/builds/actions.js';
 import LoadMoreButton from '../../../components/LoadMoreButton/LoadMoreButton.jsx';
-import Loader from '../../../components/Loader/Loader.jsx';
-import LoadingBox, {
-  Progress,
-} from '../../../components/LoadingBox/LoadingBox.jsx';
 
+import LoadingBox, { Progress } from '../../../components/LoadingBox/LoadingBox.jsx';
 import Build from './Build.jsx';
+import Link from '../../../components/Link/Link.jsx';
 
 export class Builds extends React.PureComponent {
   static propTypes = {
+    organization: PropTypes.object.isRequired,
     builds: PropTypes.arrayOf(
       PropTypes.shape({
         id: PropTypes.string.isRequired,
@@ -46,6 +41,11 @@ export class Builds extends React.PureComponent {
 
   get hasBuilds() {
     return this.props.builds.length > 0;
+  }
+
+  get snapshotsExceeded() {
+    const { organization } = this.props;
+    return organization.enforceSnapshotLimit && organization.currentSnapshotCount >= organization.monthlySnapshotLimit;
   }
 
   get currentBuildId() {
@@ -80,25 +80,32 @@ export class Builds extends React.PureComponent {
     }
     return (
       <Box fill direction="column" margin="none">
-        {this.hasBuilds && (
-          <React.Fragment>
-            <Button
-              alignSelf="end"
-              icon={<Update />}
-              onClick={this.props.onReload}
-              disabled={this.props.isLoading}
-            />
-            <Box>
-              {this.props.builds.map(build => (
-                <Build
-                  key={build.id}
-                  build={build}
-                  onChangeBuild={this.props.onChangeBuild}
-                  active={this.currentBuildId === build.id}
-                />
-              ))}
+        {this.snapshotsExceeded && (
+          <Box border={{color: 'status-warning', size: 'medium'}} round size="small" pad="medium" align="center">
+            <Box direction="row" align="center" gap="small">
+              <StatusWarning color="status-warning" />
+              <Text size="medium">You have reached the monthly limit for builds & snapshots.</Text>
             </Box>
-          </React.Fragment>
+            <Link href="/organizations" color="dark-2" label="Monthly limits" />
+          </Box>
+        )}
+        <Button
+          alignSelf="end"
+          icon={<Update />}
+          onClick={this.props.onReload}
+          disabled={this.props.isLoading}
+        />
+        {this.hasBuilds && (
+          <Box>
+            {this.props.builds.map(build => (
+              <Build
+                key={build.id}
+                build={build}
+                onChangeBuild={this.props.onChangeBuild}
+                active={this.currentBuildId === build.id}
+              />
+            ))}
+          </Box>
         )}
         {!this.hasBuilds && (
           <Box
@@ -131,6 +138,7 @@ const mapState = state => ({
   hasNextPage: getHasNextPage(state),
   builds: getBuilds(state),
   currentBuild: getCurrentBuild(state),
+  organization: getCurrentOrganization(state),
 });
 
 const mapAction = dispatch => ({
