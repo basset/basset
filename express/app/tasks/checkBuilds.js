28224f9b4bb52dd9f0361a97f41fe03919d530d6
jeremy <jeremygonzalez@outlook.com>
2019-12-04 17:08:42 -0800

    fix checkbuild

--- express/app/tasks/checkBuilds.js
+++ express/app/tasks/checkBuilds.js
@@ -3,10 +3,12 @@ const { checkBuild } = require('./builds');
 
 const run = async () => {
   console.log('[checkBuilds] checking for timed out builds');
-  const builds = await Build
-    .query()
+  const builds = await Build.query()
     .whereNull('completedAt')
-    .orWhereNot('error', true);
+    .where(builder => {
+      builder.where('error', false)
+        .orWhereNull('error');
+    });
 
   for await (const build of builds) {
     await checkBuild(build);
