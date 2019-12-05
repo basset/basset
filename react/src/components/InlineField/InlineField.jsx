d0610a4d0425d5e7bd8d7b08b54a279f73e8e146
jeremy <jeremygonzalez@outlook.com>
2019-12-04 16:44:25 -0800

    align non editable inline field

--- react/src/components/InlineField/InlineField.jsx
+++ react/src/components/InlineField/InlineField.jsx
@@ -19,7 +19,7 @@ const InlineField = React.memo(
     };
     let children;
     if (!canChange) {
-      children = <Text>{value}</Text>;
+      children = <Text alignSelf="center">{value}</Text>;
     } else if (isEditing) {
       children = (
         <Form onSubmit={handleSubmit}>
