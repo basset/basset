e9158573669c166f2c6e27c50607fb07278467a0
Jeremy Gonzalez <jeremygonzalez@outlook.com>
2019-12-11 10:49:06 +0800

    add accordion toggle to snapshot list (#84)

--- react/src/app/snapshots/components/SnapshotType.jsx
+++ react/src/app/snapshots/components/SnapshotType.jsx
@@ -59,6 +59,7 @@ class SnapshotType extends React.PureComponent {
           <Box flex>
             {this.props.type === 'modified' && this.renderApproveAll()}
           </Box>
+          {this.props.renderToggleShow}
         </Box>
       </StickyBox>
     );
