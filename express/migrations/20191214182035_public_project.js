618a72dc7c70fcd773132f00d32eb2530993a1f2
jeremy <jeremygonzalez@outlook.com>
2019-12-21 05:55:33 -0800

    add organization setting for public projects

--- express/migrations/20191214182035_public_project.js
+++ express/migrations/20191214182035_public_project.js
@@ -3,10 +3,16 @@ exports.up = async function(knex) {
   await knex.schema.table('project', table => {
     table.boolean('public').defaultTo(false).notNullable();
   });
+  await knex.schema.table('organization', table => {
+    table.boolean('allow_public_projects').defaultTo(false).notNullable();
+  });
 };
 
 exports.down = async function(knex) {
   await knex.schema.table('project', table => {
     table.dropColumn('public');
+  });
+  await knex.schema.table('organization', table => {
+    table.dropColumn('allow_public_projects')
   })
 };
