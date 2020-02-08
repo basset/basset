5656644be5b2a2aba5e27ee33bd594c70f05ff22
jeremy <jeremygonzalez@outlook.com>
2020-02-08 15:07:27 -0800

    add ability to load plugins in the backend

---
+++ express/app/plugins/index.js
@@ -0,0 +1,14 @@
+// any .js files in this folder are imported (.js files inside directories are not imported)
+const fs = require('fs').promises;
+
+module.exports = async function importPlugins(app) {
+  const files = await fs.readdir(__dirname);
+  const jsFiles = files.filter(file => file.match(/\.js$/) && !file.match(/^index.js$/));
+  for await (const file of jsFiles) {
+    console.log(`[plugin] loading ${file}`);
+    const init = require(`./${file.slice(0, -3)}`);
+    if (typeof init === 'function') {
+      await init(app);
+    }
+  }
+}
